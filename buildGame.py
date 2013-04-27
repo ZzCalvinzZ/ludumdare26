4e7e88fa82256026bde3d1b61ea75031dc61d410
Willy Collins <wcurtiscollins@gmail.com>
2013-04-27 09:38:33 +0100

    Have js builder also update dependencies afterwards

--- buildGame.py
+++ buildGame.py
@@ -7,8 +7,12 @@ from subprocess import call
 rootPath = 'ludumdare26'
 pattern = '*.coffee'
  
+print 'Compiling coffeescript files...'
 for root, dirs, files in os.walk(rootPath):
     for filename in fnmatch.filter(files, pattern):
         print( os.path.join(root, filename))
         call( [ 'coffee', '-c', os.path.join(root, filename) ] )
 
+print 'Updating all closure dependencies...'
+print os.path.join(os.getcwd(), 'bin/limejs.py')
+call([ 'bin/lime.py', 'update' ])
